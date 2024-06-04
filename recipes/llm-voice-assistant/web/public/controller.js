36b07b4c233f3720e8e3892d4fefa16a9a98c09a
Kwangsoo Yeo <kyeo@picovoice.ai>
2024-06-04 12:49:40 -0700

    LLM voice assistant for Web (#3)

---
+++ recipes/llm-voice-assistant/web/public/controller.js
@@ -0,0 +1,168 @@
+window.onload = () => {
+  let dotIdx = 0;
+  let dotInterval;
+
+  const status = document.getElementById("status");
+  const error = document.getElementById("error");
+  const dotdotdot = document.getElementById("dotdotdot");
+
+  const initBlock = document.getElementById("initBlock");
+  const chatBlock = document.getElementById("chatBlock");
+
+  const initButton = document.getElementById("init");
+  const resetDialogButton = document.getElementById("resetDialog");
+
+  const accessKey = document.getElementById("accessKey");
+  const modelFile = document.getElementById("uploadFile");
+  const message = document.getElementById("message");
+  const result = document.getElementById("result");
+
+  const writeError = (errorString) => {
+    error.style.display = 'block';
+    error.innerText = `Error: ${errorString}`;
+    status.innerText = 'Error.';
+  };
+
+  const startDot = () => {
+    if (dotInterval) {
+      clearInterval(dotInterval);
+    }
+    dotInterval = setInterval(() => {
+      dotIdx = (dotIdx + 1) % 4;
+      let dotText = '';
+      for (let i = 0; i < dotIdx; i++) {
+        dotText += '.';
+      }
+      dotdotdot.innerText = dotText;
+    }, 500);
+  }
+
+  const stopDot = () => {
+    if (dotInterval) {
+      clearInterval(dotInterval);
+    }
+    dotIdx = 0;
+    dotdotdot.innerText = '';
+  }
+
+  const startHumanMessage = () => {
+    result.innerHTML += `<div class="align-end"><span class="human-border">You</span></div>`;
+
+    const divElem = document.createElement("div");
+    divElem.className = "align-end";
+
+    const textElem = document.createElement("span");
+    textElem.className = "human-text";
+    textElem.innerHTML = "";
+
+    divElem.appendChild(textElem);
+    result.appendChild(divElem);
+
+    result.scrollTop = result.scrollHeight;
+
+    return textElem;
+  }
+
+  const startLLMMessage = () => {
+    result.innerHTML += `<div class="align-start"><span class="llm-border">picoLLM</span></div>`;
+
+    const divElem = document.createElement("div");
+    divElem.className = "align-start";
+
+    const textElem = document.createElement("span");
+    textElem.className = "llm-text";
+    textElem.innerHTML = "";
+
+    divElem.appendChild(textElem);
+    result.appendChild(divElem);
+
+    result.scrollTop = result.scrollHeight;
+
+    return textElem;
+  }
+
+  const addMessage = (elem, messageString) => {
+    elem.innerHTML += messageString;
+    result.scrollTop = result.scrollHeight;
+  };
+
+  initButton.onclick = async () => {
+    initButton.disabled = true;
+    status.innerText = "Loading model"
+    startDot();
+
+    let humanElem, llmElem;
+    let completeTranscript = "";
+    let audioStream;
+    let streamCalls = 0;
+
+    try {
+      await Picovoice.init(accessKey.value,
+        {
+          modelFile: modelFile.files[0],
+          cacheFilePath: modelFile.files[0].name,
+        },
+        {
+        onDetection: () => {
+          message.innerText = "Wake word detected, utter your request or question...";
+          humanElem = startHumanMessage();
+        },
+        onTranscript: (transcript) => {
+          completeTranscript += transcript;
+          addMessage(humanElem, transcript);
+        },
+        onEndpoint: async () => {
+          message.innerHTML = "Generating <div class='loader'></div>";
+          llmElem = startLLMMessage();
+        },
+        onText: (text) => {
+          addMessage(llmElem, text);
+        },
+        onStream: async (pcm) => {
+          audioStream.stream(pcm);
+          if (streamCalls > 1) {
+            audioStream.play();
+          } else {
+            streamCalls++;
+          }
+        },
+        onComplete: async () => {
+          if (streamCalls <= 2) {
+            audioStream.play();
+          }
+          await audioStream.waitPlayback();
+
+          await Picovoice.start();
+          message.innerText = "Say `Picovoice`"
+          streamCalls = 0;
+        },
+      });
+      initBlock.style.display = 'none';
+      chatBlock.style.display = 'flex';
+      status.innerText = "Loading complete."
+
+      await Picovoice.start();
+      message.innerText = "Say `Picovoice`"
+
+      if (!audioStream) {
+        audioStream = new Picovoice.AudioStream(Picovoice.getStreamSampleRate());
+      }
+    } catch (e) {
+      writeError(e.message);
+    } finally {
+      stopDot();
+    }
+  };
+
+  accessKey.onchange = () => {
+    if (accessKey.value.length > 0 && modelFile.files.length > 0) {
+      initButton.disabled = false;
+    }
+  }
+
+  modelFile.onchange = accessKey.onchange;
+
+  resetDialogButton.onclick = () => {
+    result.innerHTML = '';
+  }
+};
---
+++ recipes/llm-voice-assistant/web/src/main.ts
@@ -0,0 +1,195 @@
+import { Mutex } from 'async-mutex';
+
+import { BuiltInKeyword, PorcupineDetection, PorcupineWorker } from '@picovoice/porcupine-web';
+import { CheetahTranscript, CheetahWorker } from '@picovoice/cheetah-web';
+import { OrcaStreamWorker, OrcaWorker } from '@picovoice/orca-web';
+import { Dialog, PicoLLMModel, PicoLLMWorker } from '@picovoice/picollm-web';
+import { WebVoiceProcessor } from '@picovoice/web-voice-processor';
+
+type PvObject = {
+  porcupine: PorcupineWorker;
+  cheetah: CheetahWorker;
+  pllm: PicoLLMWorker;
+  dialog: Dialog;
+  orca: OrcaWorker;
+  stream: OrcaStreamWorker;
+};
+
+type PvCallback = {
+  onDetection: (detection: PorcupineDetection) => void;
+  onTranscript: (transcript: string) => void;
+  onEndpoint: () => void;
+  onText: (text: string) => void;
+  onStream: (pcm: Int16Array) => void;
+  onComplete: () => void;
+}
+
+let object: PvObject | null = null;
+
+const init = async (
+  accessKey: string,
+  picollmModel: PicoLLMModel,
+  {
+    onDetection,
+    onEndpoint,
+    onTranscript,
+    onText,
+    onStream,
+    onComplete,
+  }: PvCallback): Promise<void> => {
+  if (object !== null) {
+    return;
+  }
+
+  const detectionCallback = async (detection: PorcupineDetection): Promise<void> => {
+    if (detection.index === 0) {
+      await WebVoiceProcessor.subscribe(cheetah);
+      await WebVoiceProcessor.unsubscribe(porcupine);
+      onDetection(detection);
+    }
+  };
+
+  const porcupine = await PorcupineWorker.create(
+    accessKey,
+    BuiltInKeyword.Picovoice,
+    detectionCallback,
+    {
+      publicPath: "models/porcupine_params.pv"
+    });
+
+  const transcriptCallback = async (transcript: CheetahTranscript): Promise<void> => {
+    if (transcript.isEndpoint) {
+      await WebVoiceProcessor.unsubscribe(cheetah);
+      cheetah.flush();
+    }
+    if (transcript.transcript.length > 0) {
+      transcripts.push(transcript.transcript);
+      onTranscript(transcript.transcript);
+    }
+    if (transcript.isFlushed) {
+      onEndpoint();
+      await onCheetahFlushed();
+    }
+  };
+
+  const cheetah = await CheetahWorker.create(
+    accessKey,
+    transcriptCallback,
+    {
+      publicPath: "models/cheetah_params.pv"
+    });
+
+  const pllm = await PicoLLMWorker.create(
+    accessKey,
+    picollmModel);
+
+  const orca = await OrcaWorker.create(
+    accessKey,
+    {
+      publicPath: "models/orca_params.pv"
+    });
+
+  const dialog = pllm.getDialog();
+  const stream = await orca.streamOpen();
+
+  const mutex = new Mutex();
+  let transcripts: string[] = [];
+  let synthesized = 0;
+  let stopTokens = 0;
+
+  const stopPhrases = [
+    '</s>', // Llama-2, Mistral
+    '<end_of_turn>', // Gemma
+    '<|endoftext|>', // Phi-2
+    '<|eot_id|>', // Llama-3
+  ];
+
+  const onCheetahFlushed = async (): Promise<void> => {
+    const prompt = transcripts.join('');
+    transcripts = [];
+    dialog.addHumanRequest(prompt);
+
+    const { completion, completionTokens } = await pllm.generate(dialog.prompt(), {
+      completionTokenLimit: 128,
+      stopPhrases: stopPhrases,
+      streamCallback: async token => {
+        if (!stopPhrases.includes(token)) {
+          onText(token);
+          const release = await mutex.acquire();
+          const pcm = await stream.synthesize(token);
+          release();
+          synthesized++;
+          if (pcm !== null) {
+            onStream(pcm);
+          }
+        } else {
+          stopTokens++;
+        }
+      }
+    });
+    dialog.addLLMResponse(completion);
+
+    const waitForSynthesize = (): Promise<void> => new Promise<void>(resolve => {
+      const interval = setInterval(() => {
+        if (synthesized === (completionTokens.length - stopTokens)) {
+          clearInterval(interval);
+          resolve();
+        }
+      }, 100);
+    });
+
+    await waitForSynthesize();
+    const pcm = await stream.flush();
+    if (pcm !== null) {
+      onStream(pcm);
+    }
+    synthesized = 0;
+    stopTokens = 0;
+    onComplete();
+  };
+
+  object = {
+    porcupine,
+    cheetah,
+    pllm,
+    orca,
+    dialog,
+    stream,
+  };
+};
+
+const start = async (): Promise<void> => {
+  if (object === null) {
+    return;
+  }
+
+  await WebVoiceProcessor.subscribe(object.porcupine);
+};
+
+const getStreamSampleRate = (): number => {
+  if (object) {
+    return object.orca.sampleRate;
+  }
+  return 0;
+};
+
+const release = async (): Promise<void> => {
+  if (object === null) {
+    return;
+  }
+
+  const { porcupine, cheetah, pllm, orca } = object;
+  porcupine.terminate();
+  cheetah.terminate();
+  await pllm.release();
+  orca.terminate();
+
+  object = null;
+};
+
+export {
+  init,
+  start,
+  getStreamSampleRate,
+  release,
+};
