74461a000b31f209828fe17a6804430a88f9f0c2
Alireza Kenarsari <alireza@picovoice.ai>
2025-04-30 14:01:23 -0700

    wip

--- recipes/personal-wake-word/python/enroll.py
+++ recipes/personal-wake-word/python/enroll.py
@@ -8,7 +8,7 @@ from pveagle import EagleProfilerEnrollFeedback
 from pvrecorder import PvRecorder
 
 FEEDBACK_TO_DESCRIPTIVE_MSG = {
-    EagleProfilerEnrollFeedback.AUDIO_OK: 'Good audio',
+    EagleProfilerEnrollFeedback.AUDIO_OK: '✅',
     EagleProfilerEnrollFeedback.AUDIO_TOO_SHORT: 'Insufficient audio length',
     EagleProfilerEnrollFeedback.UNKNOWN_SPEAKER: 'Different speaker in audio',
     EagleProfilerEnrollFeedback.NO_VOICE_FOUND: 'No voice found in audio',
@@ -16,50 +16,34 @@ FEEDBACK_TO_DESCRIPTIVE_MSG = {
 }
 
 
-class EnrollmentAnimation(Thread):
-    def __init__(self, sleep_time_sec=0.1):
-        self._sleep_time_sec = sleep_time_sec
-        self._frames = [
-            " .  ",
-            " .. ",
-            " ...",
-            "  ..",
-            "   .",
-            "    "
-        ]
-        self._done = False
-        self._percentage = 0
-        self._feedback = ''
+class FeedbackAnimation(Thread):
+    def __init__(self):
         super().__init__()
 
+        self._stop = False
+        self.percentage = 0
+        self.feedback = ''
+
     def run(self):
-        self._done = False
-        while not self._done:
-            for frame in self._frames:
-                if self._done:
-                    break
-                print('\033[2K\033[1G\r[%3d%%]' % self._percentage + self._feedback + frame, end='', flush=True)
-                time.sleep(self._sleep_time_sec)
+        frames = [" .  ", " .. ", " ...", "  ..", "   .", "    "]
+        i = 0
+
+        while not self._stop:
+            if self._stop:
+                break
+
+            print(
+                f'\033[2K\033[1G\r[{self.percentage:3d}] {frames[i % len(frames)]} [{self.feedback}]',
+                end='',
+                flush=True)
+            i += 1
+            time.sleep(0.1)
+
+        self._stop = False
 
     def stop(self):
-        print('\033[2K\033[1G\r[%3d%%]' % self._percentage + self._feedback, end='', flush=True)
-        self._done = True
-
-    @property
-    def percentage(self):
-        return self._percentage
-
-    @property
-    def feedback(self):
-        return self._feedback
-
-    @percentage.setter
-    def percentage(self, value):
-        self._percentage = value
-
-    @feedback.setter
-    def feedback(self, value):
-        self._feedback = value
+        print('\033[2K\033[1G\r', end='', flush=True)
+        self._stop = True
 
 
 def main() -> None:
@@ -101,7 +85,7 @@ def main() -> None:
 
     recorder = PvRecorder(frame_length=porcupine.frame_length)
 
-    enrollment_animation = EnrollmentAnimation()
+    enrollment_animation = FeedbackAnimation()
 
     enroll_percentage = 0.0
     enrollment_animation.start()
@@ -118,14 +102,14 @@ def main() -> None:
         recorder.stop()
 
         enroll_percentage, feedback = eagle.enroll(enroll_pcm)
-        enrollment_animation.percentage = enroll_percentage
-        enrollment_animation.feedback = ' - %s' % FEEDBACK_TO_DESCRIPTIVE_MSG[feedback]
+        enrollment_animation.percentage = int(enroll_percentage)
+        enrollment_animation.feedback = FEEDBACK_TO_DESCRIPTIVE_MSG[feedback]
 
     enrollment_animation.stop()
 
     with open(eagle_speaker_profile_path, 'wb') as f:
         f.write(eagle.export().to_bytes())
-    print('\nSpeaker profile is saved to %s' % eagle_speaker_profile_path)
+    print('Speaker profile is saved to %s' % eagle_speaker_profile_path)
 
     recorder.stop()
     recorder.delete()
