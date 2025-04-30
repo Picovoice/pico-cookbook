26ad4193cc4fa7f7a4ddb7364d3b1970ca3f9030
Alireza Kenarsari <alireza@picovoice.ai>
2025-04-29 18:37:02 -0700

    wip

--- recipes/personal-wake-word/python/enroll.py
+++ recipes/personal-wake-word/python/enroll.py
@@ -2,16 +2,9 @@ import time
 from argparse import ArgumentParser
 from threading import Thread
 
-from pveagle import (
-    EagleActivationLimitError,
-    EagleError,
-    EagleProfilerEnrollFeedback,
-    create_profiler,
-)
-from pvporcupine import (
-    PorcupineError,
-)
-from pvporcupine import create as create_porcupine
+import pveagle
+import pvporcupine
+from pveagle import EagleProfilerEnrollFeedback
 from pvrecorder import PvRecorder
 
 FEEDBACK_TO_DESCRIPTIVE_MSG = {
@@ -99,71 +92,51 @@ def main() -> None:
     porcupine_sensitivity = args.porcupine_sensitivity
     eagle_speaker_profile_path = args.eagle_speaker_profile_path
 
-    try:
-        eagle_profiler = create_profiler(access_key=access_key)
-    except EagleError as e:
-        print(f"Failed to initialize Eagle: {e}")
-        return
-
-    print(f'Eagle version: {eagle_profiler.version}')
-
-    try:
-        porcupine = create_porcupine(
-            access_key=access_key,
-            model_path=porcupine_model_path,
-            keyword_paths=[porcupine_keyword_path],
-            sensitivities=[porcupine_sensitivity])
-    except PorcupineError as e:
-        print(f"Failed to initialize Porcupine wth {e}")
-        return
-    print(f"Porcupine version: {porcupine.version}")
+    eagle = pveagle.create_profiler(access_key=access_key)
+    porcupine = pvporcupine.create(
+        access_key=access_key,
+        model_path=porcupine_model_path,
+        keyword_paths=[porcupine_keyword_path],
+        sensitivities=[porcupine_sensitivity])
 
     recorder = PvRecorder(frame_length=porcupine.frame_length)
-    print(f"Recording audio from `{recorder.selected_device}`")
+
     enrollment_animation = EnrollmentAnimation()
-    print('Please keep speaking until the enrollment percentage reaches 100%')
-    try:
-        enroll_percentage = 0.0
-        enroll_index = 0
-        enrollment_animation.start()
-        while enroll_percentage < 100.0:
-            enroll_pcm = list()
-            recorder.start()
 
-            is_detected = False
-            while not is_detected:
-                frame = recorder.read()
-                enroll_pcm.extend(frame)
-                is_detected = porcupine.process(frame) == 0
+    enroll_percentage = 0.0
+    enroll_index = 0
+    enrollment_animation.start()
+    while enroll_percentage < 100.0:
+        enroll_pcm = list()
+        recorder.start()
 
-            for _ in range(8):
-                enroll_pcm.extend(recorder.read())
+        is_detected = False
+        while not is_detected:
+            frame = recorder.read()
+            enroll_pcm.extend(frame)
+            is_detected = porcupine.process(frame) == 0
 
-            recorder.stop()
+        for _ in range(8):
+            enroll_pcm.extend(recorder.read())
 
-            enroll_index += 1
-
-            enroll_percentage, feedback = eagle_profiler.enroll(enroll_pcm)
-            enrollment_animation.percentage = enroll_percentage
-            enrollment_animation.feedback = ' - %s' % FEEDBACK_TO_DESCRIPTIVE_MSG[feedback]
-
-        speaker_profile = eagle_profiler.export()
-        enrollment_animation.stop()
-        with open(eagle_speaker_profile_path, 'wb') as f:
-            f.write(speaker_profile.to_bytes())
-        print('\nSpeaker profile is saved to %s' % eagle_speaker_profile_path)
-
-    except KeyboardInterrupt:
-        print('\nStopping enrollment. No speaker profile is saved.')
-        enrollment_animation.stop()
-    except EagleActivationLimitError:
-        print('AccessKey has reached its processing limit')
-    except EagleError as e:
-        print('Failed to enroll speaker: %s' % e)
-    finally:
         recorder.stop()
-        recorder.delete()
-        eagle_profiler.delete()
+
+        enroll_index += 1
+
+        enroll_percentage, feedback = eagle.enroll(enroll_pcm)
+        enrollment_animation.percentage = enroll_percentage
+        enrollment_animation.feedback = ' - %s' % FEEDBACK_TO_DESCRIPTIVE_MSG[feedback]
+
+    speaker_profile = eagle.export()
+    enrollment_animation.stop()
+    with open(eagle_speaker_profile_path, 'wb') as f:
+        f.write(speaker_profile.to_bytes())
+    print('\nSpeaker profile is saved to %s' % eagle_speaker_profile_path)
+
+    recorder.stop()
+    recorder.delete()
+    porcupine.delete()
+    eagle.delete()
 
 
 if __name__ == '__main__':
