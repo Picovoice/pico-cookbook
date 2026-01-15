23214592bb76d773eb9f0c0405b4f31a26e2d065
Eric Mikulin <eric@picovoice.ai>
2026-01-15 11:43:22 -0800

    Rename example

--- recipes/intent-based-voice-assistant/mcu/arduino/setup.py
+++ recipes/intent-based-voice-assistant/mcu/arduino/setup.py
@@ -67,7 +67,7 @@ def generate_package(
                 porcupine_repo,
                 rhino_repo,
                 lang,
-                f"{project_folder}/examples/PicovoiceExample")
+                f"{project_folder}/examples/IntentBasedVoiceAssistant")
 
             command = f"find '{project_folder}' -type f -exec sed -i 's/{{LANGUAGE_KEYWORD}}/{keyword}/g' {{}} \\;"
             _, err = subprocess.Popen(
