944ca9b44da6d10e7a88e111877e6f7de343f907
Alireza Kenarsari <alireza@picovoice.ai>
2026-04-14 16:25:02 -0700

    mic

---
+++ recipes/live-captioning-and-translation/python/main.py
@@ -0,0 +1,233 @@
+import os
+import sys
+from argparse import ArgumentParser
+from enum import Enum
+from threading import (
+    Event,
+    Lock,
+    Thread
+)
+from time import sleep
+from typing import (
+    Callable,
+    Tuple
+)
+
+import pvcheetah
+import pvzebra
+from pvrecorder import PvRecorder
+
+
+class LanguagePairs(Enum):
+    DE_DE = "de-de"
+    DE_EN = "de-en"
+    DE_ES = "de-es"
+    DE_FR = "de-fr"
+    DE_IT = "de-it"
+    EN_DE = "en-de"
+    EN_EN = "en-en"
+    EN_ES = "en-es"
+    EN_FR = "en-fr"
+    EN_IT = "en-it"
+    ES_DE = "es-de"
+    ES_EN = "es-en"
+    ES_ES = "es-es"
+    ES_FR = "es-fr"
+    ES_IT = "es-it"
+    FR_DE = "fr-de"
+    FR_EN = "fr-en"
+    FR_ES = "fr-es"
+    FR_FR = "fr-fr"
+    IT_DE = "it-de"
+    IT_EN = "it-en"
+    IT_ES = "it-es"
+    IT_FR = "it-fr"
+    IT_IT = "it-it"
+
+
+def print_async(get_text: Callable[[], str], refresh_sec: float = 0.1, end: str = '\n') -> Tuple[Event, Thread]:
+    stop_event = Event()
+
+    def run() -> None:
+        dots_list = [" .  ", " .. ", " ...", "  ..", "   .", "    "]
+        i = 0
+
+        # Hide the cursor.
+        sys.stdout.write("\033[?25l")
+        sys.stdout.flush()
+
+        residue = 0
+
+        while not stop_event.is_set():
+            text = get_text()
+            dots = dots_list[i]
+            sys.stdout.write(f"\r{' ' * residue}")
+            sys.stdout.write(f"\r{text}{dots}")
+            sys.stdout.flush()
+            residue = len(text) + len(dots)
+
+            i = (i + 1) % len(dots_list)
+            sleep(refresh_sec)
+
+        text = get_text()
+        sys.stdout.write(f"\r{text}    {end}")
+        sys.stdout.flush()
+
+    thread = Thread(target=run, daemon=True)
+    thread.start()
+    return stop_event, thread
+
+
+def main_microphone(
+        access_key: str,
+        source_language: str,
+        target_language: str,
+        endpoint_duration_sec: float,
+        disable_automatic_punctuation: bool,
+        disable_text_normalization: bool,
+) -> None:
+    recorder = None
+    cheetah = None
+    zebra = None
+
+    try:
+        cheetah_model_path = os.path.join(
+            os.path.dirname(__file__),
+            'cheetah/lib/common',
+            'cheetah_params.pv' if source_language == 'en' else f'cheetah_params_{source_language}.pv')
+        cheetah = pvcheetah.create(
+            access_key=access_key,
+            model_path=cheetah_model_path,
+            endpoint_duration_sec=endpoint_duration_sec,
+            enable_automatic_punctuation=not disable_automatic_punctuation,
+            enable_text_normalization=not disable_text_normalization)
+        print(f"[OK] Cheetah Streaming Speech-to-Text[V{cheetah.version}][{source_language.upper()}]")
+
+        if source_language != target_language:
+            zebra_model_path = os.path.join(
+                os.path.dirname(__file__),
+                'zebra/lib/common',
+                f'zebra_params_{source_language}_{target_language}.pv')
+            zebra = pvzebra.create(access_key=access_key, model_path=zebra_model_path)
+            print(f"[OK] Zebra Translation[V{zebra.version}][{source_language.upper()} → {target_language.upper()}]")
+
+        recorder = PvRecorder(frame_length=cheetah.frame_length)
+        recorder.start()
+
+        print()
+
+        text = ""
+        text_lock = Lock()
+
+        def get_text():
+            with text_lock:
+                display_text = "(listening)" if text == "" else text
+                return f"[{source_language.upper()}] {display_text}"
+
+        text_event, text_thread = print_async(get_text)
+
+        while True:
+            partial, is_endpoint = cheetah.process(recorder.read())
+            with text_lock:
+                text += partial
+            if is_endpoint:
+                remainder = cheetah.flush()
+                with text_lock:
+                    text += remainder
+                if len(text) > 0:
+                    text_event.set()
+                    text_thread.join()
+
+                    if target_language != source_language:
+                        print(f"[{target_language.upper()}] {zebra.translate(text)}")
+
+                    print()
+                    text = ""
+                    text_event, text_thread = print_async(get_text)
+
+                    recorder.start()
+    except KeyboardInterrupt:
+        pass
+    finally:
+        # Make the cursor visible again.
+        sys.stdout.write("\033[?25h")
+        sys.stdout.flush()
+
+        if zebra is not None:
+            zebra.delete()
+
+        if cheetah is not None:
+            cheetah.delete()
+
+        if recorder is not None:
+            recorder.stop()
+            recorder.delete()
+
+
+def main_file() -> None:
+    pass
+
+
+def main() -> None:
+    parser = ArgumentParser()
+    parser.add_argument(
+        '--access_key',
+        required=True,
+        help='AccessKey obtained from Picovoice Console (https://console.picovoice.ai/)')
+    parser.add_argument(
+        '--language_pair',
+        required=True,
+        choices=[x.value for x in LanguagePairs],
+        help='Translation language pair.')
+    parser.add_argument(
+        '--audio-path',
+        help='')
+    parser.add_argument(
+        '--endpoint_duration_sec',
+        type=float,
+        default=1.0,
+        help='Duration of silence in seconds required to detect the end of an utterance.')
+    parser.add_argument(
+        '--disable_automatic_punctuation',
+        action='store_true',
+        help='Disable automatic punctuation in Streaming Speech-to-Text.')
+    parser.add_argument(
+        '--disable_text_normalization',
+        action='store_true',
+        help='Disable text normalization in Streaming Speech-to-Text.')
+    parser.add_argument(
+        '--gender',
+        choices=['female', 'male'],
+        default='male',
+        help='Voice gender to use for translated speech output.')
+    args = parser.parse_args()
+
+    access_key = args.access_key
+    language_pair = LanguagePairs(args.language_pair)
+    audio_path = args.audio_path
+    endpoint_duration_sec = args.endpoint_duration_sec
+    disable_automatic_punctuation = args.disable_automatic_punctuation
+    disable_text_normalization = args.disable_text_normalization
+    gender = args.gender
+
+    source_language, target_language = [x for x in language_pair.value.split('-')]
+
+    if source_language == target_language:
+        print(f"Transcribing `{source_language}`.")
+    else:
+        print(f"Translating from `{source_language}` to `{target_language}`.")
+
+    if audio_path is None:
+        main_microphone(
+            access_key=access_key,
+            source_language=source_language,
+            target_language=target_language,
+            endpoint_duration_sec=endpoint_duration_sec,
+            disable_automatic_punctuation=disable_automatic_punctuation,
+            disable_text_normalization=disable_text_normalization)
+    else:
+        main_file()
+
+
+if __name__ == '__main__':
+    main()
