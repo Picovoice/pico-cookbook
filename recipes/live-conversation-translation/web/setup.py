76f085aa7e02be69252d7a4e90c0999fb35ee6ae
Matthew Maxwell <matthew@picovoice.ai>
2026-04-14 13:24:48 -0700

    update

--- recipes/live-conversation-translation/web/setup.py
+++ recipes/live-conversation-translation/web/setup.py
@@ -1,6 +1,14 @@
 import os
 import shutil
 import subprocess
+import sys
+import time
+from threading import (
+    Event,
+    Thread
+)
+from typing import Tuple
+
 
 ANIMALS = [
     ("cheetah", "4.0.0"),
@@ -12,26 +20,69 @@ RENAMES = [
     ("cheetah_params.pv", "cheetah_params_en.pv")
 ]
 
+
+def animate_status(text: str) -> Tuple[Event, Thread]:
+    stop_event = Event()
+
+    def worker() -> None:
+        frames = [" .  ", " .. ", " ...", "  ..", "   .", "    "]
+        i = 0
+        while not stop_event.is_set():
+            sys.stdout.write(f'\r{text}{frames[i % len(frames)]}')
+            sys.stdout.flush()
+            i += 1
+            time.sleep(0.1)
+
+    thread = Thread(target=worker, daemon=True)
+    thread.start()
+    return stop_event, thread
+
+
 def clone_repo(animal: str, major: str, minor: str) -> str:
     folder = os.path.join(os.path.dirname(__file__), animal)
     if os.path.exists(folder):
         shutil.rmtree(folder)
 
-    subprocess.run(
-        [
-            "git",
-            "clone",
-            "--branch",
-            f"v{major}.{minor}",
-            "--depth",
-            "1",
-            f"https://github.com/Picovoice/{animal}.git",
-            folder,
-        ],
-        check=True,
-        stdout=subprocess.DEVNULL,
-        stderr=subprocess.DEVNULL,
-    )
+    stop_event, thread = animate_status(f"Cloning {animal.capitalize()}'s GitHub repository")
+
+    try:
+        subprocess.run(
+            [
+                "git",
+                "clone",
+                "--branch",
+                f"v{major}.{minor}",
+                "--depth",
+                "1",
+                "--filter=blob:none",
+                "--sparse",
+                f"https://github.com/Picovoice/{animal}.git",
+                folder,
+            ],
+            check=True,
+            stdout=subprocess.DEVNULL,
+            stderr=subprocess.DEVNULL,
+        )
+
+        subprocess.run(
+            [
+                "git",
+                "-C",
+                folder,
+                "sparse-checkout",
+                "set",
+                "lib/common",
+            ],
+            check=True,
+            stdout=subprocess.DEVNULL,
+            stderr=subprocess.DEVNULL,
+        )
+    finally:
+        stop_event.set()
+        thread.join()
+
+    sys.stdout.write(f"\rCloned {animal.capitalize()}'s GitHub repository.{' ' * 20}\n")
+    sys.stdout.flush()
 
     return folder
 
@@ -52,6 +103,7 @@ def main() -> None:
         dst_path = os.path.join(public_folder, new)
         if os.path.exists(src_path):
             os.rename(src_path, dst_path)
+
 
 if __name__ == '__main__':
     main()
