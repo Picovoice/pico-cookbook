63052ea8a4bd01aaebbe653b28dc24e0ddc0c985
Eric Mikulin <eric@picovoice.ai>
2026-04-16 13:04:29 -0700

    Initial speech to speech web demo

---
+++ recipes/speech-to-speech-translation/web/public/controller.js
@@ -0,0 +1,179 @@
+window.onload = () => {
+  let dotIdx = 0;
+  let dotInterval = null;
+
+  const status = document.getElementById("status");
+  const error = document.getElementById("error");
+  const dotdotdot = document.getElementById("dotdotdot");
+
+  const initBlock = document.getElementById("initBlock");
+  const chatBlock = document.getElementById("chatBlock");
+
+  const initButton = document.getElementById("init");
+
+  const accessKey = document.getElementById("accessKey");
+  const languagePair = document.getElementById("languagePair");
+  const changeLanguage = document.getElementById("changeLanguage");
+  const message = document.getElementById("message");
+  const result = document.getElementById("result");
+
+  let animationIndex = 0;
+  const animationElement = document.createElement("span");
+  const animationFrames = ["   ", ".  ", ".. ", "...", " ..", "  ."];
+  const animationInterval = setInterval(() => {
+    const frame = animationFrames[animationIndex];
+    animationElement.innerText = ` ${frame} `;
+    animationIndex = (animationIndex + 1) % animationFrames.length;
+  }, 500);
+
+  const state = {
+    bubbleElem: null,
+    upperElem: null,
+    lowerElem: null,
+    upperTextElem: null,
+    lowerTextElem: null,
+  };
+
+  const writeError = (errorString) => {
+    error.style.display = 'block';
+    error.innerText = `Error: ${errorString}`;
+    status.innerText = 'Error.';
+  };
+
+  const startDot = () => {
+    if (dotInterval) {
+      clearInterval(dotInterval);
+    }
+    dotInterval = setInterval(() => {
+      dotIdx = (dotIdx + 1) % 4;
+      let dotText = '';
+      for (let i = 0; i < dotIdx; i++) {
+        dotText += '.';
+      }
+      dotdotdot.innerText = dotText;
+    }, 500);
+  }
+
+  const stopDot = () => {
+    if (dotInterval) {
+      clearInterval(dotInterval);
+    }
+    dotIdx = 0;
+    dotdotdot.innerText = '';
+  }
+
+  const sendState = (mode, text) => {
+    result.scrollTop = result.scrollHeight;
+
+    if (mode === "status") {
+      status.innerText = text;
+    } else if (mode === "prompt") {
+      message.innerText = text;
+    } else if (mode === "listen") {
+      const rowElem = document.createElement("div");
+      const bubbleElem = document.createElement("div");
+      const upperElem = document.createElement("div");
+      const upperTextElem = document.createElement("span");
+
+      if (text === "right") {
+        rowElem.className = "align-end";
+      } else if (text === "left") {
+        rowElem.className = "align-start";
+      }
+
+      bubbleElem.className = "text-bubble";
+      upperElem.classList = "upper-bubble";
+
+      upperElem.appendChild(upperTextElem);
+      upperElem.appendChild(animationElement);
+      bubbleElem.appendChild(upperElem);
+      rowElem.appendChild(bubbleElem);
+      result.appendChild(rowElem);
+
+      result.scrollTop = result.scrollHeight;
+      state.bubbleElem = bubbleElem
+      state.upperElem = upperElem;
+      state.upperTextElem = upperTextElem;
+    } else if (mode === "transcript") {
+      state.upperTextElem.innerText += text;
+    } else if (mode === "translate") {
+      const lowerElem = document.createElement("div");
+      const lowerTextElem = document.createElement("span");
+      lowerElem.classList = "lower-bubble";
+      lowerElem.appendChild(lowerTextElem);
+      state.bubbleElem.appendChild(lowerElem);
+
+      state.lowerElem = lowerElem;
+      state.lowerTextElem = lowerTextElem;
+
+      state.upperElem.classList.add("mute-text");
+      state.upperElem.removeChild(animationElement);
+      state.lowerElem.appendChild(animationElement);
+    } else if (mode === "translation") {
+      state.lowerTextElem.innerText += text;
+      if (text == "") {
+        state.lowerElem.removeChild(animationElement);
+      }
+    }
+  };
+
+  const onChangeLanguage = async () => {
+    languagePair.disabled = true;
+    status.innerText = "Loading"
+    startDot();
+
+    try {
+      const pair = languagePair.value.split("-");
+      await Picovoice.release();
+      result.innerHTML = "";
+
+      const start = await Picovoice.init(
+        accessKey.value,
+        pair[0],
+        pair[1],
+        sendState
+      );
+
+      await start();
+    } catch (e) {
+      writeError(e.message);
+    } finally {
+      languagePair.disabled = false;
+      stopDot();
+    }
+  };
+
+  initButton.onclick = async () => {
+    initButton.disabled = true;
+    status.innerText = "Loading"
+    startDot();
+
+    try {
+      const pair = languagePair.value.split("-");
+
+      const start = await Picovoice.init(
+        accessKey.value,
+        pair[0],
+        pair[1],
+        sendState
+      );
+      initBlock.style.display = 'none';
+      chatBlock.style.display = 'flex';
+      status.innerText = "Loading complete.";
+      changeLanguage.appendChild(languagePair);
+      languagePair.addEventListener("change", onChangeLanguage);
+
+      await start();
+    } catch (e) {
+      writeError(e.message);
+    } finally {
+      stopDot();
+    }
+  };
+
+  accessKey.onchange = () => {
+    if (accessKey.value.length > 0) {
+      initButton.disabled = false;
+    }
+  }
+};
---
+++ recipes/speech-to-speech-translation/web/src/index.ts
@@ -0,0 +1,212 @@
+import { CheetahTranscript, CheetahWorker } from '@picovoice/cheetah-web';
+import { WebVoiceProcessor } from '@picovoice/web-voice-processor';
+import { ZebraWorker } from '@picovoice/zebra-web';
+import { OrcaWorker } from '@picovoice/orca-web';
+import { AudioStream } from './audio_stream';
+
+type PvObject = {
+  audio: AudioStream,
+  cheetah0: CheetahWorker,
+  cheetah1: CheetahWorker,
+  zebra0: ZebraWorker,
+  zebra1: ZebraWorker,
+  orca0: OrcaWorker,
+  orca1: OrcaWorker,
+  transcript: string,
+  translation: string,
+};
+
+let object: PvObject | null = null;
+
+const init = async (
+  accessKey: string,
+  sourceLanguage: string,
+  targetLanguage: string,
+  sendState: (mode: string, text: string) => void,
+): Promise<() => Promise<void>> => {
+
+  let mode: boolean = false;
+  const startListening = async () => {
+    const language = mode ? targetLanguage : sourceLanguage;
+    const side = mode ? "left" : "right";
+    const cheetah = mode ? object!.cheetah1 : object!.cheetah0;
+
+    await WebVoiceProcessor.subscribe(cheetah);
+    sendState("prompt", `Listening for ${language}`);
+    sendState("listen", side);
+  };
+
+  const stopListening = async () => {
+    const cheetah = mode ? object!.cheetah1: object!.cheetah0;
+
+    await WebVoiceProcessor.unsubscribe(cheetah);
+  };
+
+  const startTranslating = async () => {
+    const language = mode ? sourceLanguage : targetLanguage;
+    const side = mode ? "left" : "right";
+    const zebra = mode ? object!.zebra1 : object!.zebra0;
+
+    const transcript = object!.transcript;
+    object!.transcript = "";
+
+    sendState("prompt", `Translating to ${language}`);
+    sendState("translate", "");
+    const translation = await zebra.translate(transcript);
+    object!.translation = translation;
+  };
+
+  const startSpeaking = async () => {
+    const language = mode ? sourceLanguage : targetLanguage;
+    const orca = mode ? object!.orca0 : object!.orca1;
+
+    sendState("prompt", `Synthesizing ${language} speech`)
+    const translation = object!.translation;
+    const synthesis = await orca.synthesize(translation, {});
+    const alignments = synthesis.alignments;
+
+    sendState("prompt", `Speaking in ${language}`)
+    object!.audio.stream(synthesis.pcm);
+    object!.audio.play();
+
+    for (let alignment of alignments) {
+      const index = alignments.indexOf(alignment);
+      let word = alignment.word;
+      if (index < alignments.length - 1 &&
+          !(/[.,:;!?]/.test(alignments[index + 1].word))
+      ) {
+        word += " ";
+      }
+
+      setTimeout(() => {
+        sendState("translation", word);
+      }, alignment.startSec * 1000);
+    }
+
+    await object!.audio.waitPlayback();
+    sendState("translation", "");
+  };
+
+  const switchModes = async () => {
+    mode = !mode;
+  }
+
+  const transcriptCallback = async (
+    transcript: CheetahTranscript
+  ): Promise<void> => {
+    if (transcript.transcript.length > 0) {
+      sendState("transcript", transcript.transcript);
+      object!.transcript += transcript.transcript;
+    }
+
+    if (transcript.isEndpoint && object!.transcript.length > 0) {
+      await stopListening();
+      await startTranslating();
+      await startSpeaking();
+      await switchModes();
+      await startListening();
+    }
+  }
+
+  sendState("status", `Loading cheetah_${sourceLanguage}`);
+  const cheetah0 = await CheetahWorker.create(
+    accessKey,
+    transcriptCallback,
+    {
+      publicPath: `models/cheetah_params_${sourceLanguage}.pv`,
+      customWritePath: `cheetah_${sourceLanguage}`
+    },
+    {
+      endpointDurationSec: 1.0,
+      enableAutomaticPunctuation: true,
+      enableTextNormalization: true
+    }
+  );
+
+  sendState("status", `Loading cheetah_${targetLanguage}`);
+  const cheetah1 = await CheetahWorker.create(
+    accessKey,
+    transcriptCallback,
+    {
+      publicPath: `models/cheetah_params_${targetLanguage}.pv`,
+      customWritePath: `cheetah_${targetLanguage}`
+    },
+    {
+      endpointDurationSec: 1.0,
+      enableAutomaticPunctuation: true,
+      enableTextNormalization: true
+    }
+  );
+
+  sendState("status", `Loading zebra_${sourceLanguage}_${targetLanguage}`);
+  const zebra0 = await ZebraWorker.create(
+    accessKey,
+    {
+      publicPath: `models/zebra_params_${sourceLanguage}_${targetLanguage}.pv`,
+      customWritePath: `zebra_${sourceLanguage}_${targetLanguage}`
+    }
+  );
+
+  sendState("status", `Loading zebra_${targetLanguage}_${sourceLanguage}`);
+  const zebra1 = await ZebraWorker.create(
+    accessKey,
+    {
+      publicPath: `models/zebra_params_${targetLanguage}_${sourceLanguage}.pv`,
+      customWritePath: `zebra_${targetLanguage}_${sourceLanguage}`
+    }
+  );
+
+  sendState("status", `Loading orca_${sourceLanguage}`);
+  const orca0 = await OrcaWorker.create(
+    accessKey,
+    {
+      publicPath: `models/orca_params_${sourceLanguage}_male.pv`,
+      customWritePath: `orca_${sourceLanguage}`
+    },
+    {}
+  )
+
+  sendState("status", `Loading orca_${targetLanguage}`);
+  const orca1 = await OrcaWorker.create(
+    accessKey,
+    {
+      publicPath: `models/orca_params_${targetLanguage}_male.pv`,
+      customWritePath: `orca_${targetLanguage}`
+    },
+    {}
+  )
+  const audio = new AudioStream(orca0.sampleRate);
+
+  object = {
+    audio: audio,
+    cheetah0: cheetah0,
+    cheetah1: cheetah1,
+    zebra0: zebra0,
+    zebra1: zebra1,
+    orca0: orca0,
+    orca1: orca1,
+    transcript: "",
+    translation: "",
+  };
+
+  return startListening;
+};
+
+const release = async () => {
+  WebVoiceProcessor.reset();
+  object!.audio.clear();
+
+  object!.cheetah0.release();
+  object!.cheetah1.release();
+  object!.zebra0.release();
+  object!.zebra1.release();
+  object!.orca0.release();
+  object!.orca1.release();
+
+  object = null;
+}
+
+export default {
+  init,
+  release
+};
