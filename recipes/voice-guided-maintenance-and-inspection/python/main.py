5611c22b33ec838d1b97c4e29c38ec98eef1ea60
Alireza Kenarsari <alireza@picovoice.ai>
2026-04-19 18:18:03 -0700

    wip

---
+++ recipes/voice-guided-maintenance-and-inspection/python/main.py
@@ -0,0 +1,21 @@
+class Workflow(object):
+    pass
+
+
+class Step(object):
+    pass
+
+class RhinoStep(Step):
+    pass
+
+
+class CheetahStep(Step):
+    pass
+
+
+def main() -> None:
+    pass
+
+
+if __name__ == '__main__':
+    main()
