27ed96dadb4256ff6d876917b783ffbe231e7e1d
Alireza Kenarsari <alireza@picovoice.ai>
2026-04-20 06:13:23 -0700

    wip

--- recipes/voice-guided-maintenance-and-inspection/python/main.py
+++ recipes/voice-guided-maintenance-and-inspection/python/main.py
@@ -1,9 +1,57 @@
+from typing import (
+    Optional,
+    Sequence
+)
+
+import pvporcupine
+from pvporcupine import Porcupine
+
+
 class Workflow(object):
     pass
 
 
 class Step(object):
-    pass
+    def __init__(
+            self,
+            name: Optional[str] = None,
+    ) -> None:
+        self._name = name
+
+    def run(self) -> None:
+        raise NotImplementedError()
+
+    def __str__(self) -> str:
+        raise NotImplementedError()
+
+
+class PorcupineStep(Step):
+    def __init__(
+            self,
+            name: Optional[str] = None,
+            access_key: Optional[str] = None,
+            model_path: Optional[str] = None,
+            keyword_paths: Optional[Sequence[str]] = None,
+            sensitivities: Optional[Sequence[float]] = None,
+            porcupine: Optional[Porcupine] = None
+    ) -> None:
+        super().__init__(name=name)
+
+        if porcupine is None:
+            self._porcupine = pvporcupine.create(
+                access_key=access_key,
+                model_path=model_path,
+                keyword_paths=keyword_paths,
+                sensitivities=sensitivities)
+        else:
+            self._porcupine = porcupine
+
+    def run(self) -> None:
+        pass
+
+    def __str__(self) -> str:
+        raise NotImplementedError()
+
 
 class RhinoStep(Step):
     pass
