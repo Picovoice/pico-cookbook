50eba652bf6906e490a0838b59625ce67e72c04a
Alireza Kenarsari <alireza@picovoice.ai>
2026-04-29 12:47:52 -0700

    wip

---
+++ recipes/image-qa/python/main.py
@@ -0,0 +1,13 @@
+import picollm
+import pvcheetah
+import pvorca
+from pvrecorder import PvRecorder
+from pvspeaker import PvSpeaker
+
+
+def main() -> None:
+    pass
+
+
+if __name__ == "__main__":
+    main()
