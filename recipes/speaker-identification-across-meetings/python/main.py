230242f8279bd37fb7f5c7cd1f72139d54b35396
Alireza Kenarsari <alireza@picovoice.ai>
2026-05-15 14:29:44 -0700

    wip

--- recipes/speaker-identification-across-meetings/python/main.py
+++ recipes/speaker-identification-across-meetings/python/main.py
@@ -46,64 +46,90 @@ def main() -> None:
     if unknown_speaker_profiles_folder is None:
         unknown_speaker_profiles_folder = os.path.dirname(audio_path)
 
-    falcon = pvfalcon.create(access_key=access_key)
-    print(f"[OK] Falcon Speaker Diarization[V{falcon.version}]")
-
+    falcon = None
     eagle_recognizer = None
     eagle_profiler = None
-    if len(known_speaker_profile_paths) > 0:
-        eagle_recognizer = pveagle.create_recognizer(access_key=access_key)
-        print(f"[OK] Eagle Speaker Recognition[V{eagle_recognizer.version}]")
-    if profile_unknown_speakers:
-        eagle_profiler = pveagle.create_profiler(access_key=access_key)
-        if eagle_recognizer is None:
-            print(f"[OK] Eagle Speaker Recognition[V{eagle_profiler.version}]")
 
-    pcm, sample_rate = soundfile.read(audio_path, dtype='int16', always_2d=True)
-    if sample_rate != falcon.sample_rate:
-        raise RuntimeError()
-    pcm = pcm[:, 0]
+    try:
+        falcon = pvfalcon.create(access_key=access_key)
+        print(f"[OK] Falcon Speaker Diarization[V{falcon.version}]")
 
-    segments = falcon.process(pcm)
+        if len(known_speaker_profile_paths) > 0:
+            eagle_recognizer = pveagle.create_recognizer(access_key=access_key)
+            print(f"[OK] Eagle Speaker Recognition[V{eagle_recognizer.version}]")
+        if profile_unknown_speakers:
+            eagle_profiler = pveagle.create_profiler(access_key=access_key)
+            if eagle_recognizer is None:
+                print(f"[OK] Eagle Speaker Recognition[V{eagle_profiler.version}]")
 
-    speaker_tag_map = dict()
-    if eagle_recognizer is not None:
-        speaker_pcms = dict()
+        print()
+
+        pcm, sample_rate = soundfile.read(audio_path, dtype='int16', always_2d=True)
+        if sample_rate != falcon.sample_rate:
+            raise RuntimeError()
+        pcm = pcm[:, 0]
+
+        segments = falcon.process(pcm)
+
+        speaker_tag_map = dict()
+        if eagle_recognizer is not None:
+            speaker_pcms = dict()
+            for x in segments:
+                if x.speaker_tag not in speaker_pcms:
+                    speaker_pcms[x.speaker_tag] = list()
+                speaker_pcms[x.speaker_tag].extend(pcm[int(x.start_sec * sample_rate): int(x.end_sec * sample_rate)])
+
+            known_speaker_profiles = list()
+            known_speaker_names = list()
+            for x in known_speaker_profile_paths:
+                with open(x, 'rb') as f:
+                    known_speaker_profiles.append(EagleProfile.from_bytes(f.read()))
+                known_speaker_names.append(os.path.splitext(os.path.basename(x))[0])
+
+            for speaker, speaker_pcm in speaker_pcms.items():
+                similarities = eagle_recognizer.process(
+                    pcm=speaker_pcm,
+                    speaker_profiles=known_speaker_profiles)
+
+                best_index = max(range(len(similarities)), key=lambda i: similarities[i])
+                best_similarity = similarities[best_index]
+
+                if best_similarity >= similarity_threshold:
+                    speaker_tag_map[speaker] = known_speaker_names[best_index]
+                    print(f"Identified speaker `{speaker}` as `{known_speaker_names[best_index]}`")
+
+        if len(speaker_tag_map) > 0:
+            print()
+
+        if eagle_profiler is not None:
+            speaker_intervals = dict()
+            for x in segments:
+                if x.speaker_tag not in speaker_tag_map:
+                    if x.speaker_tag not in speaker_intervals:
+                        speaker_intervals[x.speaker_tag] = list()
+                    speaker_intervals[x.speaker_tag].append((int(x.start_sec * sample_rate), int(x.end_sec * sample_rate)))
+
+            for speaker, intervals in speaker_intervals.items():
+                for start_sample, end_sample in intervals:
+                    for i in range(start_sample, end_sample, eagle_profiler.frame_length):
+                        eagle_profiler.enroll(pcm=pcm[i:i + eagle_profiler.frame_length])
+                percentage = eagle_profiler.flush()
+                if percentage == 100.:
+                    with open(os.path.join(unknown_speaker_profiles_folder, f"{speaker}.bin"), 'wb') as f:
+                        f.write(eagle_profiler.export().to_bytes())
+
+        print("Diarization:")
         for x in segments:
-            if x.speaker_tag not in speaker_pcms:
-                speaker_pcms[x.speaker_tag] = list()
-            speaker_pcms[x.speaker_tag].extend(pcm[int(x.start_sec * sample_rate): int(x.end_sec * sample_rate)])
+            print(f"[{speaker_tag_map.get(x.speaker_tag, x.speaker_tag)}] {x.start_sec:.2f} → {x.end_sec:.2f}")
+    finally:
+        if eagle_profiler is not None:
+            eagle_profiler.delete()
 
-        known_speaker_profiles = list()
-        for x in known_speaker_profile_paths:
-            with open(x, 'rb') as f:
-                known_speaker_profiles.append(EagleProfile.from_bytes(f.read()))
+        if eagle_recognizer is not None:
+            eagle_recognizer.delete()
 
-        for speaker, speaker_pcm in speaker_pcms.items():
-            similarities = eagle_recognizer.process(
-                pcm=speaker_pcm,
-                speaker_profiles=known_speaker_profiles)
-            print(similarities)
-
-    if eagle_profiler is not None:
-        speaker_intervals = dict()
-        for x in segments:
-            if x.speaker_tag not in speaker_tag_map:
-                if x.speaker_tag not in speaker_intervals:
-                    speaker_intervals[x.speaker_tag] = list()
-                speaker_intervals[x.speaker_tag].append((int(x.start_sec * sample_rate), int(x.end_sec * sample_rate)))
-
-        for speaker, intervals in speaker_intervals.items():
-            for start_sample, end_sample in intervals:
-                for i in range(start_sample, end_sample, eagle_profiler.frame_length):
-                    eagle_profiler.enroll(pcm=pcm[i:i + eagle_profiler.frame_length])
-            percentage = eagle_profiler.flush()
-            if percentage == 100.:
-                with open(os.path.join(unknown_speaker_profiles_folder, f"{speaker}.bin"), 'wb') as f:
-                    f.write(eagle_profiler.export().to_bytes())
-
-    for x in segments:
-        print(f"[{speaker_tag_map.get(x.speaker_tag, x.speaker_tag)}] {x.start_sec:.2f} → {x.end_sec:.2f}")
+        if falcon is not None:
+            falcon.delete()
 
 
 if __name__ == '__main__':
