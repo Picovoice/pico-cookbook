ae0c2327a859cab9bc996c0080ab662461bab0fc
Matthew Maxwell <matthew@picovoice.ai>
2026-05-19 13:47:00 -0700

    pre

---
+++ recipes/voice-guided-maintenance-and-inspection/ios/VoiceGuidedMaintenanceAndInspection/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  VoiceGuidedMaintenanceAndInspection
+//
+//  Created by picovoice on 2026-05-19.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ recipes/voice-guided-maintenance-and-inspection/ios/VoiceGuidedMaintenanceAndInspection/VoiceGuidedMaintenanceAndInspectionApp.swift
@@ -0,0 +1,17 @@
+//
+//  VoiceGuidedMaintenanceAndInspectionApp.swift
+//  VoiceGuidedMaintenanceAndInspection
+//
+//  Created by picovoice on 2026-05-19.
+//
+
+import SwiftUI
+
+@main
+struct VoiceGuidedMaintenanceAndInspectionApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
