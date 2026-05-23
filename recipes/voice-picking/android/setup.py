a964585deaca22d7174f6ce6761ff990a48fd827
Gabriel Stang <gabriel@picovoice.ai>
2026-05-22 17:24:51 -0700

    initial commit (everything but mainactivity)

---
+++ recipes/voice-picking/android/setup.py
@@ -0,0 +1,144 @@
+import os
+import shutil
+import subprocess
+import sys
+import time
+from argparse import ArgumentParser
+from threading import (
+    Event,
+    Thread
+)
+from typing import Tuple
+
+
+ANIMALS = [
+    ("koala", "3.0.0"),
+    ("orca", "3.0.0"),
+]
+
+COPIES = {
+    "koala": [
+        ("koala_params.pv", "koala_params.pv")
+    ],
+    "orca": [
+        ("orca_params_en_female.pv", "orca_params_en_female.pv")
+    ],
+}
+
+
+def animate_status(text: str) -> Tuple[Event, Thread]:
+    stop_event = Event()
+
+    def worker() -> None:
+        frames = [" .  ", " .. ", " ...", "  ..", "   .", "    "]
+        i = 0
+        while not stop_event.is_set():
+            sys.stdout.write(f'\r{text}{frames[i % len(frames)]}')
+            sys.stdout.flush()
+            i += 1
+            time.sleep(0.1)
+
+    thread = Thread(target=worker, daemon=True)
+    thread.start()
+    return stop_event, thread
+
+
+def clone_repo(animal: str, major: str, minor: str) -> str:
+    folder = os.path.join(os.path.dirname(__file__), animal)
+    if os.path.exists(folder):
+        shutil.rmtree(folder)
+
+    stop_event, thread = animate_status(f"Cloning {animal.capitalize()}'s GitHub repository")
+
+    try:
+        subprocess.run(
+            [
+                "git",
+                "clone",
+                "--branch",
+                f"v{major}.{minor}",
+                "--depth",
+                "1",
+                "--filter=blob:none",
+                "--sparse",
+                f"https://github.com/Picovoice/{animal}.git",
+                folder,
+            ],
+            check=True,
+            stdout=subprocess.DEVNULL,
+            stderr=subprocess.DEVNULL,
+        )
+
+        subprocess.run(
+            [
+                "git",
+                "-C",
+                folder,
+                "sparse-checkout",
+                "set",
+                "lib/common",
+            ],
+            check=True,
+            stdout=subprocess.DEVNULL,
+            stderr=subprocess.DEVNULL,
+        )
+    finally:
+        stop_event.set()
+        thread.join()
+
+    sys.stdout.write(f"\rCloned {animal.capitalize()}'s GitHub repository.{' ' * 20}\n")
+    sys.stdout.flush()
+
+    return folder
+
+
+def main() -> None:
+    parser = ArgumentParser()
+    parser.add_argument(
+        '--access_key',
+        required=True,
+        help='`AccessKey` obtained from `Picovoice Console` (https://console.picovoice.ai/).')
+    parser.add_argument(
+        '--porcupine_keyword_path',
+        required=True,
+        help='Absolute path to the Porcupine model file (`.ppn`).')
+    parser.add_argument(
+        '--rhino_context_path',
+        required=True,
+        help='Absolute path to the Rhino model file (`.rhn`).')
+    args = parser.parse_args()
+
+    public_folder = os.path.join(
+        os.path.dirname(__file__),
+        "voice-picking",
+        "src",
+        "main",
+        "assets")
+    for animal, version in ANIMALS:
+        major, minor = version.split('.')[:2]
+        folder = clone_repo(animal, major, minor)
+        model_folder = os.path.join(folder, "lib", "common")
+        for filename in COPIES[animal]:
+            src_path = os.path.join(model_folder, filename)
+            dst_path = os.path.join(public_folder, filename)
+            shutil.copy(src_path, dst_path)
+
+    shutil.copy(args.porcupine_keyword_path, os.path.join(public_folder, "voice_picking_android.ppn"))
+    shutil.copy(args.rhino_context_path, os.path.join(public_folder, "voice_picking_android.rhn"))
+
+    main_activity_path = os.path.join(
+        os.path.dirname(__file__),
+        "voice-picking",
+        "src",
+        "main",
+        "java/ai/picovoice/voicepicking",
+        "MainActivity.java")
+    with open(main_activity_path, 'r') as main_activity_file:
+        main_activity_contents = main_activity_file.read()
+    main_activity_contents = main_activity_contents.replace("${YOUR_ACCESS_KEY_HERE}", args.access_key)
+    with open(main_activity_path, 'w') as main_activity_file:
+        main_activity_file.write(main_activity_contents)
+
+
+if __name__ == '__main__':
+    main()
